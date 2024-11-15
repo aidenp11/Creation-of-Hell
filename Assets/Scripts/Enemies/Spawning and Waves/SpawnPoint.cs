4e0b520780f4574e2ebe12c5018f575489ffe2ba
aidenp11 <129340716+aidenp11@users.noreply.github.com>
2024-11-14 22:09:10 -0700

    bruhhhhhh

--- Assets/Scripts/Enemies/Spawning and Waves/SpawnPoint.cs
+++ Assets/Scripts/Enemies/Spawning and Waves/SpawnPoint.cs
@@ -2,21 +2,5 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    [SerializeField] GameObject enemyToSpawn;
-    [SerializeField] float secondsToSpawn;
-	private float ogseconds;
-
-	private void Start()
-	{
-		ogseconds = secondsToSpawn;
-	}
-	private void Update()
-	{
-		secondsToSpawn -= Time.deltaTime;
-		if (secondsToSpawn <= 0 )
-		{
-			Instantiate(enemyToSpawn, transform);
-			secondsToSpawn = ogseconds;
-		}
-	}
+    
 }
---
+++ Assets/Scripts/Enemies/Spawning and Waves/Waves.cs
@@ -0,0 +1,92 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Waves : MonoBehaviour
+{
+    [SerializeField] List<GameObject> spawnPoints;
+    [SerializeField] GameObject mmf;
+    [SerializeField] GameObject huggyBear;
+    [SerializeField] GameObject jumpster;
+
+	private GameObject player;
+	private Transform playerTransform;
+
+	private int roundNumber;
+    public int mmfCount;
+    private int originalmmfCount;
+    public int huggyBearCount;
+    private int originalHuggyBearCount;
+    public int jumpsterCount;
+    private int originalJumpsterCount;
+	private int totalSpawned;
+
+	private float timeBetweenSpawns = 1;
+
+	private int spawnNumber = 0;
+
+	private void Start()
+	{
+		for (int i = 0; i < SceneManager.GetActiveScene().GetRootGameObjects().Length; i++)
+		{
+			if (SceneManager.GetActiveScene().GetRootGameObjects().ElementAt(i).GetComponent<PlayerMovement2D>())
+			{
+				player = SceneManager.GetActiveScene().GetRootGameObjects().ElementAt(i);
+				break;
+			}
+		}
+	}
+
+	private void Update()
+	{
+		playerTransform = player.GetComponent<Transform>();
+
+		for (int i = spawnNumber; i < spawnPoints.Count; )
+		{ 
+			if (Mathf.Abs(spawnPoints.ElementAt(i).transform.position.x - playerTransform.position.x) <= 30 && timeBetweenSpawns <= 0)
+			{
+				if (mmfCount > 0)
+				{
+					Instantiate(mmf, spawnPoints.ElementAt(i).transform);
+					mmfCount--;
+				}
+				timeBetweenSpawns = 1;
+				if (spawnNumber < spawnPoints.Count)
+				{
+					spawnNumber++;
+				}
+				else
+				{
+					spawnNumber = 0;
+				}
+			}
+			else
+			{
+				if (spawnNumber < spawnPoints.Count)
+				{
+					spawnNumber++;
+				}
+				else
+				{
+					spawnNumber = 0;
+				}
+			}
+		}
+
+		if (timeBetweenSpawns >= 0)
+		{
+			timeBetweenSpawns -= Time.deltaTime;
+		}
+		else
+		{
+			timeBetweenSpawns = 0;
+		}
+	}
+
+	private void RoundChange()
+	{
+
+	}
+}
