783fe62ef9a1e163b369306acb54aedfb6fd533d
aidenp11 <129340716+aidenp11@users.noreply.github.com>
2024-12-19 16:40:51 -0700

    skins system and miku

--- Assets/Scripts/GameManager/TitleManager.cs
+++ Assets/Scripts/GameManager/TitleManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 public class TitleManager : MonoBehaviour
 {
 	[SerializeField] Texture2D cursor;
+	[SerializeField] IntVariable skinNum;
+	[SerializeField] GameObject skinmenu;
 	private void Start()
 	{
 		Cursor.SetCursor(cursor, new Vector2(0, 50), CursorMode.ForceSoftware);
@@ -18,5 +20,25 @@ public class TitleManager : MonoBehaviour
 	public void OnQuitButtonClick()
 	{
 		Application.Quit();
+	}
+
+	public void OnSoldierButton()
+	{
+		skinNum.value = 0;
+	}
+
+	public void OnMikuButton()
+	{
+		skinNum.value = 1;
+	}
+
+	public void OnBackButton()
+	{
+		skinmenu.SetActive(false);
+	}
+
+	public void OnSkinButton()
+	{
+		skinmenu.SetActive(true);
 	}
 }
---
+++ Assets/Scripts/Player/Skins.cs
@@ -0,0 +1,32 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Skins : MonoBehaviour
+{
+    [SerializeField] IntVariable skinNum;
+
+    [SerializeField] GameObject arm;
+
+    [SerializeField] Sprite soldier;
+    [SerializeField] Sprite soldierHand;
+    [SerializeField] Sprite miku;
+    [SerializeField] Sprite mikuHand;
+    [SerializeField] AnimatorController soldierController;
+    [SerializeField] AnimatorController mikuController;
+
+	private void Start()
+	{
+        if (skinNum == 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = soldier;
+            arm.GetComponent<SpriteRenderer>().sprite = soldierHand;
+            GetComponent<Animator>().runtimeAnimatorController = soldierController;
+        }
+        else if (skinNum == 1)
+        {
+			GetComponent<SpriteRenderer>().sprite = miku;
+			arm.GetComponent<SpriteRenderer>().sprite = mikuHand;
+			GetComponent<Animator>().runtimeAnimatorController = mikuController;
+		}
+	}
+}
